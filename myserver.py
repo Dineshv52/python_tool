ecb26e1c1869b38b50bd33176237d9626e91199a
dineshv52 <dineshv52@yahoo.in>
2019-04-03 23:24:09 +0530

    update

---
+++ myserver.py
@@ -0,0 +1,14 @@
+#First of all we are importing the socket library
+import socket
+
+#next create a scocket object
+
+s = socket.socket()
+print("socket suucessfulyy created")
+
+#reserve  a port number
+port = 12345
+
+
+
+
